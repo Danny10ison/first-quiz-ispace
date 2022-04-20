18a68905d8bd4c155805ee0505e5491e3df15b80
Danny10ison <pagesngod@gmail.com>
2022-04-20 07:29:33 +0000

    starting

--- app/src/main/java/com/hgecapsi/newsapptestoneproject/ui/activities/MainActivity.kt
+++ app/src/main/java/com/hgecapsi/newsapptestoneproject/ui/activities/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //  Handle bottom navigation
+        //  Handle bottom naviga
         bottomNavigationView = findViewById(R.id.nav_view)
 
         bottomNavigationView.setOnNavigationItemSelectedListener{
